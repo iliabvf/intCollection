6bd0d319107ef5f956785173bd3d2aac9e8ab246
COMPUTER\vasek <vasika@gmail.com>
2019-06-23 13:42:09 +0300

    3

--- src/net/iliabvf/initCollection/InitCollection.java
+++
@@ -1,9 +0,0 @@
-package net.iliabvf.initCollection;
-
-public class InitCollection {
-
-    public static void main(String[] args)
-    {
-
-    }
-}
---
+++ src/net/iliabvf/intCollection/IntCollection.java
@@ -0,0 +1,9 @@
+package net.iliabvf.intCollection;
+
+public class IntCollection {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
