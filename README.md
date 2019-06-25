b9bf62248c4e07b6b9a2e7c915242232d7bfeafd
COMPUTER\vasek <vasika@gmail.com>
2019-06-25 03:00:35 +0300

    divided into 3 classes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
     1.git clone
     2.Open in IDEA
-    3.Run class IntCollection
+    3.Run class Main
---
+++ src/net/iliabvf/intCollection/Main.java
@@ -0,0 +1,8 @@
+package net.iliabvf.intCollection;
+
+public class Main {
+    public static void main(String[] args){
+        BusinessLogic businessLogic = new BusinessLogic();
+        businessLogic.start();
+    }
+}
